e0bc1481d099e530a125d32950e745dc67e5497f
t-arf <20336460+t-arf@users.noreply.github.com>
2020-10-24 16:19:38 +0500

    Create 4.sql

---
+++ Course 2/Lab 3 Loading data into BigQuery/4.sql
@@ -0,0 +1,11 @@
+#standardSQL find the longest distance traveled in the month of January:
+CREATE TABLE
+  nyctaxi.january_trips AS
+SELECT
+  *
+FROM
+  nyctaxi.2018trips
+WHERE
+  EXTRACT(Month
+  FROM
+    pickup_datetime)=1;
